2aec1e02abbe18f1fc17a4235ec44533848ef868
SHUO0904 <1345920997@qq.com>
2022-06-19 20:40:20 +0800

    +build文件夹

---
+++ src/utils/request.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import {Message,MessageBox} from 'element-ui'
+import store from '../store'
+import {getToken}  from '@/utils/auth'
+
+//创建axios实例
+const service = axios.create({
+    baseURL:process.env.BASE_API, //api的base_url
+    timeout: 15000  //请求超时时间
+})
+
+//request拦截器
+service.interceptors.request.use(config =>{
+    if (store.getters.token){
+        config.headers['Authorization'] = getToken()  //让
+    }
+})
