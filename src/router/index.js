c456f2c1741143bae6775a9e5068cf4e5da15034
SHUO0904 <1345920997@qq.com>
2022-06-21 19:50:16 +0800

    0621

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,30 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export const constantRouteMap =[
-    {path:'/login',component:() => import('@/views/login/index'),hidden:true},
-    {path:"/404",component:() =>import('@/views/404'),hidden: true}
+export const constantRouteMap = [
+  {path: '/login', component: () => import('@/views/login/index'), hidden: true},
+  {path: '/404', component: () => import('@/views/404'), hidden: true}
 ]
 export default new Router({
   scrollBehavior: () => ({y: 0}),
   routes: constantRouteMap
 })
+
+
+export const asyncRouterMap = [
+  {
+    path: '/pms',
+    component: Layout,
+    redirect: '/pms/product',
+    name: 'pms',
+    meta: {title: '商品', icon: 'product'},
+    children: [{
+      path: 'product',
+      name: 'pms',
+      component: () => import('@/views/pms/product/index'),
+      meta: {title: '商品列表', icon: 'product-list'}
+    }]
+  }
+
+
+]
